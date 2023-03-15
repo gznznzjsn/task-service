cc82b3412583cb0d95a7273f527d95a683387a9d
gznznzjsn <arik2046@gmail.com>
2023-03-15 12:42:35 +0300

    Refactored

--- src/main/java/com/gznznzjsn/taskservice/persistence/repository/TaskRepository.java
+++ src/main/java/com/gznznzjsn/taskservice/persistence/repository/TaskRepository.java
@@ -2,9 +2,7 @@ package com.gznznzjsn.taskservice.persistence.repository;
 
 import com.gznznzjsn.taskservice.domain.Task;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TaskRepository extends R2dbcRepository<Task, Long> {
 
 }
